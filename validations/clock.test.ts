fcabcfec2f43e3742f4c1fbb59e2ec871064439e
Charlie Andrews <charlie.andrews@valoraapp.com>
2022-11-03 17:42:45 -0700

    feat(zod): check response schemas with zod
    Bitmama's API was passing a test but gave a FiatAccountType where it should have had a FiatAccountSchema, and numbers where it needed strings. This change adds regression coverage for those bugs.

--- validations/clock.test.ts
+++ validations/clock.test.ts
@@ -5,6 +5,7 @@ import { FIFTEEN_MINUTES_IN_MS } from '../src/constants'
 import path from 'path'
 import { chaiPlugin } from 'api-contract-validator'
 import { checkResponseSchema } from '../src/check-response-schema'
+import { clockResponseSchema } from '@fiatconnect/fiatconnect-types'
 
 const apiDefinitionsPath = path.join(config.openapiSpec)
 use(chaiPlugin({ apiDefinitionsPath }))
@@ -17,7 +18,7 @@ describe('/clock', () => {
     })
     const response = await client.get(`/clock`)
     expect(response).to.have.status(200)
-    checkResponseSchema(response)
+    checkResponseSchema(response, clockResponseSchema)
     const serverTimeStr = response.data?.time
     expect(!!serverTimeStr).to.be.true
     const serverTime = new Date(serverTimeStr)
