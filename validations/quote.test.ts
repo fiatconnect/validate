74a17231d5aea4e385cf2f3781b89a5511881524
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-04-14 11:00:10 -0700

    fix: make quote-in-mock and quote-out-mock optional, but require at least one

--- validations/quote.test.ts
+++ validations/quote.test.ts
@@ -7,7 +7,10 @@ import { config } from '../src/config'
 import { checkResponseSchema } from '../src/check-response-schema'
 import { MOCK_QUOTE } from '../src/mock-data/quote'
 import { ethers } from 'ethers'
-import { quoteResponseSchema } from '@fiatconnect/fiatconnect-types'
+import {
+  QuoteRequestBody,
+  quoteResponseSchema,
+} from '@fiatconnect/fiatconnect-types'
 
 const apiDefinitionsPath = path.join(config.openapiSpec)
 use(chaiPlugin({ apiDefinitionsPath }))
@@ -17,33 +20,48 @@ const headers: AxiosRequestHeaders = config.clientApiKey
   : {}
 
 describe('/quote', () => {
-  describe('/out', () => {
-    const wallet = ethers.Wallet.createRandom()
-    const quoteParams = {
-      ...MOCK_QUOTE[config.quoteOutMock],
-      address: wallet.address,
-    }
-    it('gives quote with quoteId when it should', async () => {
+  const { quoteOutMock, quoteInMock } = config
+  const wallet = ethers.Wallet.createRandom()
+  const cases: { quoteParams: QuoteRequestBody; quoteType: 'in' | 'out' }[] = []
+  if (quoteInMock) {
+    cases.push({
+      quoteParams: { ...MOCK_QUOTE[quoteInMock], address: wallet.address },
+      quoteType: 'in',
+    })
+  }
+  if (quoteOutMock) {
+    cases.push({
+      quoteParams: { ...MOCK_QUOTE[quoteOutMock], address: wallet.address },
+      quoteType: 'out',
+    })
+  }
+  it.each(cases)(
+    'gives quote with quoteId for transfer $quoteType',
+    async ({ quoteParams, quoteType }) => {
       const client = axios.create({
         baseURL: config.baseUrl,
         validateStatus: () => true,
         headers,
       })
-      const response = await client.post(`/quote/out`, quoteParams)
+      const response = await client.post(`/quote/${quoteType}`, quoteParams)
       expect(response).to.have.status(200)
       expect(response.data.quote.quoteId).not.to.be.equal('')
       checkResponseSchema(response, quoteResponseSchema)
-    })
-    it('Doesnt support quotes for unreasonably large transfer out', async () => {
+    },
+  )
+
+  it.each(cases)(
+    'Doesnt support quotes for unreasonably large transfer $quoteType',
+    async ({ quoteParams, quoteType }) => {
       const client = axios.create({
         baseURL: config.baseUrl,
         validateStatus: () => true,
         headers,
       })
       quoteParams.cryptoAmount = Number.MAX_VALUE.toString()
-      const response = await client.post(`/quote/out`, quoteParams)
+      const response = await client.post(`/quote/${quoteType}`, quoteParams)
       expect(response).to.have.status(400)
       expect(response.data.error).to.be.equal('CryptoAmountTooHigh')
-    })
-  })
+    },
+  )
 })
