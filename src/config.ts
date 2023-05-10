169ecc2381b6f09b004154b8676f37a8781b6316
Charlie Andrews <charlie.andrews@valoraapp.com>
2023-05-10 11:32:44 -0700

    destructure but avoid camelCasing magic

--- src/config.ts
+++ src/config.ts
@@ -71,8 +71,8 @@ export const config = yargs
     example: 'personalDataAndDocumentsNigeria',
     choices: Object.keys(MOCK_KYC),
   })
-  .check((argv) => {
-    if (!argv.quoteOutMock && !argv.quoteInMock) {
+  .check(({'quote-in-mock': quoteInMock, 'quote-out-mock': quoteOutMock}) => {
+    if (!quoteOutMock && !quoteInMock) {
       throw new Error(
         'Must specify at least one of quote-in-mock or quote-out-mock',
       )
