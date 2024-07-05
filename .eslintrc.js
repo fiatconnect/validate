430fa0d2c48787cf208a512d8308e5a10b89aac8
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-07-04 22:07:17 -0600

    fix: tsconfig warning at the top of validation files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: ['@valora/eslint-config-typescript'],
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
   rules: {
     'no-console': ['error', { allow: ['none'] }],
   },
